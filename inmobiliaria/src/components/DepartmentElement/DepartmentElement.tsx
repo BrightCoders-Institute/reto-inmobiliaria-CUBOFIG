ca0a0a9ae3253ceae300b443d3bbd36edcc358f1
CUBOFIG <figmiche@gmail.com>
2023-06-16 02:10:38 -0600

    feature/structure

---
+++ inmobiliaria/src/components/DepartmentElement/DepartmentElement.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DepartmentElement = () => {
+  return <div>DepartmentElement</div>;
+};
+
+export default DepartmentElement;
---
+++ inmobiliaria/src/components/ListElements/ListElements.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ListElements = () => {
+  return <div>ListElements</div>;
+};
+
+export default ListElements;
